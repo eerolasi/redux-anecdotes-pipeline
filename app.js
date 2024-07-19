176300afbf4c59c4f0d3fcb873890efc1cb19a9d
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-07-19 18:44:08 +0300

    back to working

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const middlewares = jsonServer.defaults({
 
 const port = process.env.PORT || 3001
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) {
-    throw new Error('Health check failed')
-  }
   res.send('ok')
 })
 app.use(middlewares)

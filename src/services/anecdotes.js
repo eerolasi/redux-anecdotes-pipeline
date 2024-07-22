c3f58e11a3f8afa855bd3b3be0f0add5bcc5104a
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-07-22 08:49:44 +0300

    fix urls

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? '/anecdotes'
+    : 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
